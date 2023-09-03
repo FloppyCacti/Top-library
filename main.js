368ed40384f1832f559dfb8b651be173722d9edd
Parth <116512352+parthp0u0@users.noreply.github.com>
2023-09-03 14:57:25 -0500

    add working form

--- main.js
+++ main.js
@@ -18,9 +18,39 @@ function addBookToLibrary() {
 
 const addBook = document.querySelector('.add-book');
 const bookForm = document.querySelector('#bookForm');
+const titleValue = document.querySelector('#bookTitle');
+const authorValue = document.querySelector('#bookAuthor');
+const pageValue = document.querySelector('#bookPages');
+const haveReadValue = document.querySelector('#haveRead');
+const cancelValue = document.querySelector('#cancelBtn')
 
 addBook.addEventListener('click', () => {
   bookForm.showModal();
+})
+
+bookForm.addEventListener('submit',(event) => {
+  event.preventDefault();
+
+  const title = titleValue.value;
+  const author = authorValue.value;
+  const page = pageValue.value;
+
+  if(title && author){
+    const newBook = new Book(title, author, page)
+    addBookToLibrary();
+
+    titleValue.value = '';
+    authorValue = '';
+    pageValue = '';
+
+  }
+  bookForm.close();
+})
+
+cancelValue.addEventListener('click', (event) =>{
+  event.preventDefault();
+
+  bookForm.close();
 })
 
 addBookToLibrary();
